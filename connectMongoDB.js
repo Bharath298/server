be5ab8cc6804e690333c57a1fe762abd407dfb8e
Bharath298 <158034534+Bharath298@users.noreply.github.com>
2024-05-27 19:47:16 +0530

    Update connectMongoDB.js

--- connectMongoDB.js
+++ connectMongoDB.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb+srv://2211cs010324:2211cs010324@cluster0.e7ljsiz.mongodb.net/week12?retryWrites=true&w=majority&appName=Cluster0");
+    await mongoose.connect(process.env.MONGODB_CONNECT_UR);
     console.log("Connect to MongoDB successfully");
   } catch (error) {
     console.log("Connection failed");
